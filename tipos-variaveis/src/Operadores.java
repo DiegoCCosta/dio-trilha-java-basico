641917c800bd0e4909d5d4a826d38660e50113e6
Diego da Conceição Costa <131775142+DiegoCCosta@users.noreply.github.com>
2024-05-08 10:02:31 -0300

    sla

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,13 @@
+public class Operadores {
+    public static void main(String[] args) {
+        int a, b;
+        a = 5;
+        b = 6;
+
+        // esse tipo de operação é so se for booleana (true ou false)
+
+        String resultado = a == b ? "Verdadeiro" : "falso";
+
+        System.out.println(resultado);
+    }
+}
